14fcc63fad1962127d5a1e8248ea98895871cc49
Aries <me@ariesclark.com>
2025-06-11 18:39:57 -0600

    fix: rust.tsx icon error

--- src/lib/icons/rust.tsx
+++ src/lib/icons/rust.tsx
@@ -2,10 +2,10 @@ export function Rust() {
 	return (
 		<svg viewBox="0 0 106 106" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 			<g transform="translate(53 53)">
-				<path d="M-8.5-14.5h13c8 0 8 8 0 8h-13Zm-31 37h40v-11h-9v-8h10c11 0 5 19 14 19h25v-19h-6v2c0 8-9 7-10 2s-5-9-6-9c15-8 6-24-6-24h-47v11h10v26h-15Z" stroke="#000" strokeLineJoin="round" />
+				<path d="M-8.5-14.5h13c8 0 8 8 0 8h-13Zm-31 37h40v-11h-9v-8h10c11 0 5 19 14 19h25v-19h-6v2c0 8-9 7-10 2s-5-9-6-9c15-8 6-24-6-24h-47v11h10v26h-15Z" stroke="#000" strokeLinejoin="round" />
 				<g mask="url(#a)">
 					<circle fill="none" r="43" stroke="#000" strokeWidth="9" />
-					<path d="m46 3 5-3-5-3z" id="b" stroke="#000" strokeLineJoin="round" strokeWidth="3" />
+					<path d="m46 3 5-3-5-3z" id="b" stroke="#000" strokeLinejoin="round" strokeWidth="3" />
 					<use transform="rotate(11)" xlinkHref="#b" />
 					<use transform="rotate(23)" xlinkHref="#b" />
 					<use transform="rotate(34)" xlinkHref="#b" />
@@ -37,7 +37,7 @@ export function Rust() {
 					<use transform="rotate(326)" xlinkHref="#b" />
 					<use transform="rotate(338)" xlinkHref="#b" />
 					<use transform="rotate(349)" xlinkHref="#b" />
-					<path d="m-7-42 7 7 7-7z" id="c" stroke="#000" strokeLineJoin="round" strokeWidth="6" />
+					<path d="m-7-42 7 7 7-7z" id="c" stroke="#000" strokeLinejoin="round" strokeWidth="6" />
 					<use transform="rotate(72)" xlinkHref="#c" />
 					<use transform="rotate(144)" xlinkHref="#c" />
 					<use transform="rotate(216)" xlinkHref="#c" />
